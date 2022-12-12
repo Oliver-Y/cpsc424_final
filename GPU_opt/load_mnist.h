4f1e95a0e5cd9a826e3171f67b656bb84361d65a
Oliver-Y <oliver.ye@yale.edu>
2022-12-12 11:36:24 -0500

    optimized

---
+++ GPU_opt/load_mnist.h
@@ -0,0 +1,12 @@
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void load_mnist(vector<float>& x_train, vector<float>& y_train, int *data_size);
