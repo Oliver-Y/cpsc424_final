b3a10ada04571756392530fa8dfa0cf083c86b9a
Jonathan Yin <jonathan.yin@yale.edu>
2022-12-12 00:11:29 -0500

    add relu

--- GPU/gpu.cu
+++ GPU/gpu.cu
@@ -179,6 +179,14 @@ void linear_update_cpu(float *in, float *errors, float *weights, float *bias, in
     }
 }
 
+__global__ void relu_forward_gpu(float *in, float *out, int n_out){
+    int index = blockDim.x*blockIdx.x + threadIdx.x;
+    if (index < n_out * BATCH_SIZE){
+        out[index] = fmaxf(0.2*in[index], in[index]);
+    }
+}
+
+
 void relu_forward_cpu(float *in, float *out, int n_out) {
     for (int j = 0; j < n_out * BATCH_SIZE; j++) {
         if (in[j] > 0) {
@@ -272,14 +280,15 @@ int main() {
 
     int n_block_rows = (BATCH_SIZE-1) / BLOCK_SIZE + 1;
     int l1_block_cols = (n_hidden - 1) / BLOCK_SIZE +1;
-
     int l2_block_cols = (n_out - 1) / BLOCK_SIZE +1;
+    int relu_blocks = (n_hidden * BATCH_SIZE - 1) / BLOCK_SIZE+1;
 
     dim3 l1_grid(n_block_rows, l1_block_cols);
     dim3 l2_grid(n_block_rows, l2_block_cols);
 
     dim3 Block(BLOCK_SIZE, BLOCK_SIZE);
 
+    cout << "row: "<< n_block_rows << " l1_cols: " << l1_block_cols << " l2_col: " << l2_block_cols << endl;
     // float *l1_weights = new float[n_in * n_hidden];
     // float *l1_bias = new float[n_hidden];
 
@@ -301,9 +310,9 @@ int main() {
     cudaMallocManaged(&l1_out, n_hidden * BATCH_SIZE*sizeof(float));
     cudaMallocManaged(&l2_out, n_out * BATCH_SIZE*sizeof(float));
     cudaMallocManaged(&relu_out, n_hidden * BATCH_SIZE*sizeof(float));
-    cudaMallocManaged(&input, n_in * data_size*sizeof(float));
+    cudaMallocManaged(&input, n_in * BATCH_SIZE*sizeof(float));
 
-    set_eq(input, &x_train[0], n_in * data_size);
+    // set_eq(input, &x_train[0], n_in * data_size);
 
     float forward_time = 0, backprop_time = 0;
     chrono::steady_clock::time_point begin, end;
@@ -311,12 +320,14 @@ int main() {
 
     begin = chrono::steady_clock::now();
     cout << "===TRAINING===" << endl;
+    // set_eq(input, &x_train[0], n_in * BATCH_SIZE);
 
     for (int i = 0; i < n_epochs; i++) {
         cout << "Epoch " << i << "\n";
         for (int batch = 0; batch < train_test_split / BATCH_SIZE; batch++) {
             // input = &x_train[batch * BATCH_SIZE * n_in];
             target = &y_train[batch * BATCH_SIZE * n_out];
+            set_eq(input, &x_train[batch * BATCH_SIZE * n_in], n_in * BATCH_SIZE);
 
             // FORWARD PROPAGATION STEP
 
@@ -324,25 +335,20 @@ int main() {
 
             // l1_out = new float[n_hidden * BATCH_SIZE];
             // linear_forward_cpu(input, l1_out, l1_weights, l1_bias, n_in, n_hidden);
-            linear_forward_gpu<<<l1_grid, Block>>>(&input[batch * BATCH_SIZE * n_in], l1_out, l1_weights, l1_bias, n_in, n_hidden);
-            cudaDeviceSynchronize();
+            linear_forward_gpu<<<l1_grid, Block>>>(input, l1_out, l1_weights, l1_bias, n_in, n_hidden);
+            // cudaDeviceSynchronize();
 
             // relu_out = new float[n_hidden * BATCH_SIZE];
-            relu_forward_cpu(l1_out, relu_out, n_hidden);
+            // relu_forward_cpu(l1_out, relu_out, n_hidden);
+            relu_forward_gpu<<<relu_blocks, BLOCK_SIZE>>>(l1_out, relu_out, n_hidden);
 
             // l2_out = new float[BATCH_SIZE * n_out];
             // linear_forward_cpu(relu_out, l2_out, l2_weights, l2_bias, n_hidden, n_out);
             linear_forward_gpu<<<l2_grid, Block>>>(relu_out, l2_out, l2_weights, l2_bias, n_hidden, n_out);
             cudaDeviceSynchronize();
 
-
             output = new float[BATCH_SIZE * n_out];
             softmax_forward_cpu(l2_out, output, n_out);
-
-            // for(int k = 0; k < 10; k++){
-            //     cout << output[k]<< " ";
-            // }
-            // cout<<"\n";
 
             error = 0;
             CE_forward_cpu(target, output, &error, n_out);
@@ -356,11 +362,6 @@ int main() {
 
             l2_error = new float[BATCH_SIZE * n_out]();
             softmax_CE_backprop_cpu(target, output, l2_error, n_out);
-
-            // for(int k = 0; k < 10; k++){
-            //     cout << l2_error[k]<< " ";
-            // }
-            // cout<<"\n";
 
             relu_error = new float[n_hidden * BATCH_SIZE]();
             linear_backprop_cpu(l2_error, relu_error, l2_weights, n_hidden, n_out);
